19f5e64ce1933eac1cb84530ce063af913842b35
NeuralVFX <vfxhoneybadger@gmail.com>
2018-08-17 15:31:47 +0545

    Putting together Usage file

--- data/get_test_dataset.sh
+++ data/get_test_dataset.sh
@@ -3,4 +3,4 @@
 URL=http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
 TAR_FILE=./data/VOCtrainval_06-Nov-2007.tar
 wget $URL -O $TAR_FILE
-tar -xvzf $TAR_FILE -C ./data/
+tar -xvf $TAR_FILE -C ./data/
