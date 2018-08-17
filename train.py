6cd123b8d3f5702204424fcb34ae42c026acc593
NeuralVFX <vfxhoneybadger@gmail.com>
2018-08-17 11:35:29 +0545

    Setting up command line

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ parser.add_argument('--load_state', nargs='?', type=str)
 params = vars(parser.parse_args())
 
 
-# if load_state arg is not used, then train model from scratch#
+# if load_state arg is not used, then train model from scratch
 if __name__ == '__main__':
     style_transfer = StyleTransfer(params)
     if params['load_state']:
